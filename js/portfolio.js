6236ad9836484eddc27aacfb0046e7122023f043
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-22 23:02:19 +0100

    fixed linters errors

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,25 +1,23 @@
 /** Set up mobile menu start */
 
-const menuOverlay = document.querySelector("#menu-overlay");
-const mobileMenuButton = document.querySelector("#mobile-menu-btn");
+const menuOverlay = document.querySelector('#menu-overlay');
+const mobileMenuButton = document.querySelector('#mobile-menu-btn');
 
 function mobileMenuToogle() {
-  alert('mobile menu toggle');
-  menuOverlay.style.width = "100%";
+  menuOverlay.style.width = '100%';
 }
 
 function closeMobileMenu() {
-  menuOverlay.style.width = "0";
+  menuOverlay.style.width = '0';
 }
 
-mobileMenuButton.addEventListener("click", () => {
-  alert('i got here');
+mobileMenuButton.addEventListener('click', () => {
   mobileMenuToogle();
 });
 
-const mobileMenuLinks = document.querySelectorAll(".menu-mobile li a");
+const mobileMenuLinks = document.querySelectorAll('.menu-mobile li a');
 for (let i = 0; i < mobileMenuLinks.length; i += 1) {
-  mobileMenuLinks[i].addEventListener("click", () => {
+  mobileMenuLinks[i].addEventListener('click', () => {
     closeMobileMenu();
   });
 }
